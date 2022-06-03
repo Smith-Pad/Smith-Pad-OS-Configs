90c205d69218b2945f5df918c5161b92b448bb59
senalbulumulle <senal.bulumulle@gmail.com>
2022-06-03 15:59:50 +0000

    Added file: php-start.sh

---
+++ php-start.sh
@@ -0,0 +1,12 @@
+#################################################
+#	start.sh
+#
+#
+#
+#
+#
+#	Simple Script to start PHP for the 
+#       Smith-Pad Web Interface
+#################################################
+
+php -S localhost:3000
