56643f6c5c10ea6871abe2ed14d73515d5c2f7ec
senalbulumulle <senal.bulumulle@gmail.com>
2022-06-05 13:51:36 -0400

    mmmm....

--- configure.sh
+++ configure.sh
@@ -4,21 +4,6 @@
 #
 #######################################################
 
-
-sudo su 							## Get the user to be prompted to enter the root password 
-								## to before installing the Smith-Pad-OS-Configs in 
-								## superuser mode
-
-
-
-git clone https://github.com/Smith-Pad/Smith-Pad-OS-Configs	## Then clone the Smith-Pad-OS-Configs Repository
-
-
-cd Smith-Pad-OS-Configs						## Then change the directory to Smith-Pad-OS-Configs
-
-
-
-
 #########################################################
 #	Then copy all of the systemd files to
 #		/etc/systemd/system
