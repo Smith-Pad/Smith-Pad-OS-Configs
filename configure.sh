dc40155a2636cf314ed6f344511ab509b5e70041
senalbulumulle <senal.bulumulle@gmail.com>
2022-06-08 22:21:21 -0400

    Gotta restart all over again../../..

--- configure.sh
+++ configure.sh
@@ -19,71 +19,7 @@ cp php-start.service 	/etc/systemd/system					## Copy to /etc/systemd/system
 
 
 
-#########################################################
-#	Then copy all of the bin files to
-#		/usr/bin
-#
-#
-#########################################################                                                    	## Change directory to usr/bin
 
-
-cp web-interface-php-start /usr/bin				## Copy the web-interface-php-start to /usr/bin
-
-
-cp web-interface.start /usr/bin					## Copy the web-interface.start to /usr/bin
-
-
-cp web-interface.stop /usr/bin					## Copy the web-interface.stop to /usr/bin
-
-
-
-
-#################################################################
-#	Then copy all of the var/Smith-Pad-OS-Shell files to
-#		/var/Smith-Pad-OS-Shell
-#
-#
-#################################################################
-
-
-cp -R Smith-Pad-OS-Shell  /var/					## Copy the files to /var/Smith-Pad-OS-Shell
-
-
-
-
-#################################################################
-#	Then copy all of the xdg files to 
-#		/etc/xdg/openbox
-#
-#
-#################################################################
-
-
-
-cp -R openbox /etc/xdg/	                                 	## Copy the files to /etc/xdg/
-
-
-
-
-#################################################################
-#	Then get [give-root.sh] the ability to turn on 
-#       all root permissions via /etc/sudoers
-#################################################################
-sh give-root.sh
-
-
-#################################################################
-#	Then enable the service daemons
-#################################################################
-
-systemctl enable php-start.service
-
-
-
-
-#################################################################
-#       Reboot the system
-#################################################################
 
 clear
 
