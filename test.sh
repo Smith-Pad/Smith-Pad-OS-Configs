796ee6f6baa51aba8e82c9f6d534d22cb3160a96
senalbulumulle <senal.bulumulle@gmail.com>
2022-06-10 12:19:21 -0400

    Added a test systemd daemon along with a test script

---
+++ test.sh
@@ -0,0 +1,13 @@
+###############################################################
+#		test.sh
+#
+#
+#
+#
+#	This script is used to test out a script being 
+#	called by a systemd daemon
+###############################################################
+
+
+
+echo "hello world" >> IT_WORKS.TXT
