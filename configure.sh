a0d43c144cee95be3d29ca5c1fb5bd705a15fafe
senalbulumulle <senal.bulumulle@gmail.com>
2022-06-11 10:03:02 -0400

    You know, I have to make the code decoration perfect... right?

--- configure.sh
+++ configure.sh
@@ -24,9 +24,12 @@ cp test.service /etc/systemd/system 		## Copy to /etc/systemd/system
 ##		Copy bin files to 
 ##		/usr/bin
 ##
-###################################################### 
+####################################################### 
 
 cp test.sh /usr/bin				## Copy to /usr/bin
+
+
+
 
 
 
