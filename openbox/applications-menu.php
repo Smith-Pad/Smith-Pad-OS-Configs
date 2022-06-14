94c52c0690dcd2cc40bb574b4ce05994caac2d40
senalbulumulle <senal.bulumulle@gmail.com>
2022-06-14 10:00:08 -0400

    Merged from [https://github.com/Smith-Pad/Smith-Pad-OS-Shell]

--- openbox/applications-menu.php
+++ openbox/applications-menu.php
@@ -35,7 +35,7 @@
                 <a href="EXTERNAL_ROUTE_APPS/web-browser.php" class="button">Web Browser</a>
                 <a href="EXTERNAL_ROUTE_APPS/file-manager.php" class="button">File Manager</a>
                 <a href="EXTERNAL_ROUTE_APPS/terminal.php" class="button ">Terminal</a> 
-                <a href="EXTERNAL_ROUTE_APPS/lxqt-config.php " class="button">System-Settings</a>            
+                <a href="EXTERNAL_ROUTE_APPS/lxqt-config.php" class="button">System-Settings</a>            
                 <?php include "src/avakasaya/space7.php"?> 
         </div>
 
