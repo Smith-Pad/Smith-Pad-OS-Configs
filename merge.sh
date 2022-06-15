62d6ec169ab0b4ae57c8c6a705c83a3ae16f2fea
senalbulumulle <senal.bulumulle@gmail.com>
2022-06-15 08:20:57 -0400

    Updated merge.sh

--- merge.sh
+++ merge.sh
@@ -29,3 +29,33 @@ git add Smith-Pad-OS-Shell
 git commit -m "Merged from [https://github.com/Smith-Pad/Smith-Pad-OS-Shell]"
 
 git push origin main
+
+
+
+
+rm -rf External-Apps-Launcher
+
+git rm External-Apps-Launcher
+
+git commit -m "Updating....."
+
+git push origin main
+
+
+
+
+
+
+git clone https://github.com/Smith-Pad/External-Apps-Launcher
+
+cd External-Apps-Launcher
+
+rm -rf .git/
+
+cd ../
+
+git add External-Apps-Launcher
+
+git commit -m "Merged from [https://github.com/Smith-Pad/External-Apps-Launcher]"
+
+git push origin main
