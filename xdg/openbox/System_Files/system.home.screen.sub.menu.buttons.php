278e82349df59d84c3178908fa5d463e2262d8a8
senalbulumulle <senal.bulumulle@gmail.com>
2022-06-30 19:23:59 +0000

    Added [xdg] configurations

---
+++ xdg/openbox/System_Files/system.home.screen.sub.menu.buttons.php
@@ -0,0 +1,14 @@
+<!----------------------------------------------------------------
+    system.home.screen.sub.menu.banner.php
+
+    Used to display the sub menu with buttons in the home screen
+---------------------------------------------------------------->
+
+<a href="#settings" class="button">Settings</a>
+<a href="schedule-display.php" class="button">Schedule Display</a>
+<a href="Math-Operations.php" class="button">Math Operations</a>
+<a href="./Subjects/Subjects.php" class="button">Subjects</a>
+<a href="./APPS/Clock/index.php" class="button">Clock Mode</a>
+<a href="applications-menu.php" class="button">Applications</a>
+<a href="http://localhost:4000" class="button">Test-Menu</a>
+<a href="External-Apps-Launcher/index.php" class="button">Test-Menu Another</a>
