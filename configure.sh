b05f8b5515a56d7e29fdcbfe35e1328989f7f6e1
senalbulumulle <senal.bulumulle@gmail.com>
2022-07-04 14:01:14 -0400

    Re-written configure.sh script

--- configure.sh
+++ configure.sh
@@ -5,99 +5,59 @@
 #######################################################
 
 
-
-
-
+#######################################################
+## Make a function to clear the screen before proceeding
+## to the next steps.
+#######################################################	
+function clearScreen() {
+	clear
+}
 
 #######################################################
-##	Clear the screen before proceeding 
-##	to install the Smith-Pad-OS-Configs
-#######################################################
-
-
-clear
-
-
-
+## Make a function that copies the systemd service
+## daemon files to /etc/systemd/system
+#######################################################	
+function copyServices() {
+	cp php-start-3000.service	/etc/systemd/system
+	cp test.service 		/etc/systemd/system
+}
 
 #######################################################
-##	Copy openbox config to /etc/xdg/
-#######################################################
-
-cp -R openbox /etc/xdg
-
-
-
-
-
+## Make a function that copies the scripts
+## /usr/bin
+#######################################################	
+function copyScripts() {
+	cp php-start-3000.sh 	/usr/bin
+	cp test.sh 		/usr/bin
+}
 
 #######################################################
-##		Copy systemd daemons to 
-##		/etc/systemd/system
-##
-##
-####################################################### 
-
-cp test.service /etc/systemd/system 				## Copy to /etc/systemd/system
-cp php-start-3000.service /etc/systemd/system			## Copy to /etc/systemd/system
-
-
-
-
-
-
-
-
-
+## Make a function to enable systemd service the first
+## time. 
+#######################################################	
+function enableServices() {
+	systemctl enable php-start-3000.service
+	systemctl enable test.service
+}
 
 #######################################################
-##		Copy bin files to 
-##		/usr/bin
-##
-####################################################### 
-
-cp test.sh /usr/bin					## Copy to /usr/bin
-cp php-start-3000.sh /usr/bin				## Copy to /usr/bin
-
-
-
-
-#######################################################
-##		Disable systemd daemons (prevent conflict)
-####################################################### 
-
-systemctl disable test.service 
-systemctl disable php-start-3000.service
-
-
-
-#######################################################
-##		Enable systemd daemons
-####################################################### 
-
-systemctl enable test.service 
-systemctl enable php-start-3000.service
-
-
-
-
-
-
-
-
-
-#######################################################
-##		Notify the user to manually 
-## 		reboot the system
-####################################################### 
-
-
-
-echo """
-	#######################################################
-	##	The configuration is completed please proceed 
-	##	to manually reboot the system by typing this 
-	##	command: [reboot]
+## Make a function to restart the system
+#######################################################	
+function rebootSystem() {
+	clear
+	echo """
+	#####################################################
+	##	 	Rebooting the System
 	##
-	#######################################################
-"""
+	#####################################################
+	"""
+	sleep 5
+	reboot
+}
+
+
+clearScreen
+copyServices
+copyScripts
+enableServices
+rebootSystem
